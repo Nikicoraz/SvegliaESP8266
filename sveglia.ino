3c23ef528a45dad3733ca783aee3ab7f798de676
Nikicoraz <nicolacorato05@gmail.com>
2023-11-29 22:20:18 +0100

    Merge menu

--- sveglia.ino
+++ sveglia.ino
@@ -15,6 +15,7 @@
 #include <ESP8266WiFi.h>
 #include "secrets.h"
 #include <WiFiUdp.h>
+#include "menu.h"
 
 const char* SSID = SECRET_SSID;
 const char* PASSWD = SECRET_PASSWD;
@@ -29,6 +30,7 @@ byte hours = 0;
 byte day = 0;
 
 char daysOfTheWeek[7][12] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+byte alarmTimes[7][2] = { {0,0} };
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
@@ -36,11 +38,30 @@ NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", utcOffsetInSeconds);
 
 // LCD setup
 const byte columns = 20;
-LiquidCrystal_I2C lcd(0x27, columns, 4);
+const byte rows = 4;
+LiquidCrystal_I2C lcd(0x27, columns, rows);
 byte isBacklightOn = 0;
 
 // Menu
-const String menu[4] { "<- Back", "Test1", "Test2", "Test3" };
+MenuItem* currentMenu = nullptr;
+int currentMenuLength;
+int menuOption = 0;
+int firstMenuOption = 0;
+byte isMenuOpen = false;
+int genericCouter;
+bool genericCount = false;
+bool genericDelay = false;
+
+//
+// --- GENERAL FUNCTIONS ---
+//
+
+void millisDelay(int delay){
+  unsigned long temp = millis();
+  while(temp - millis() < delay) {
+  }
+  return;
+}
 
 int calculateCenterTextColumnStart(int length) {
   return (columns - length) / 2;
@@ -49,7 +70,6 @@ int calculateCenterTextColumnStart(int length) {
 int calculateCenterTextColumnStart(String text) {
   return calculateCenterTextColumnStart(text.length());
 }
-
 
 void centerPrint(String text, int row = 0) {
   lcd.setCursor(calculateCenterTextColumnStart(text), row);
@@ -73,33 +93,183 @@ void connectWifi() {
   Serial.println(SSID);
 }
 
+void closeMenu(){
+  isMenuOpen = false;
+  drawMainScreen();
+}
+
 void updateNTPTime() {
   Serial.println("Updating time...");
   Serial.printf("Was %02d:%02d:%02d", hours, minutes, seconds);
 
   connectWifi();
-  timeClient.begin();
-  do{
-    timeClient.update();
-    seconds = timeClient.getSeconds();
-    minutes = timeClient.getMinutes();
-    hours = timeClient.getHours();
-    day = timeClient.getDay();
-    Serial.printf("Is now %02d:%02d:%02d", hours, minutes, seconds);
-  }while(hours == 1 && seconds == 6); // When the update fails it sets hours = 1 and seconds = 6 ???? I should change it later...
-
+timeClient.begin();
+  while(!timeClient.update()){} // Retry until the update succeds
+  seconds = timeClient.getSeconds();
+  minutes = timeClient.getMinutes();
+  hours = timeClient.getHours();
+  day = timeClient.getDay();
+  Serial.printf("Is now %02d:%02d:%02d", hours, minutes, seconds);
 
   timeClient.end();
   WiFi.disconnect();
 }
 
-void renderMenu(){
+void renderMenu(MenuItem* menu, int firstOption, bool resetCursor = true) {
   lcd.clear();
-  for(int i = 0; i < 4; i++){
-    lcd.setCursor(0, i);
-    lcd.print(menu[i]);
+  for (int i = firstOption; i < firstOption + 4; i++) {
+    lcd.setCursor(0, i - firstOption);
+    lcd.print(menu[i].getText().c_str());
+    if (i == firstOption + 3) {
+      lcd.setCursor(columns - 1, i - firstOption);
+      lcd.write(0);
+    }
+  }
+  if(resetCursor){
+    lcd.cursor();
+    lcd.setCursor(0, 0);
   }
 }
+
+void drawMainScreen(){
+  lcd.clear();
+  lcd.noCursor();
+
+  // The length of the time is always 8 chars with seconds
+  lcd.setCursor(calculateCenterTextColumnStart(8), 0);
+  lcd.printf("%02d:%02d:%02d", hours, minutes, seconds);
+}
+
+void changeMenu(MenuItem* menu, int length){
+  currentMenu = menu;
+  currentMenuLength = length;
+  isMenuOpen = true;
+  menuOption = 0;
+  renderMenu(currentMenu, 0);
+}
+
+ICACHE_RAM_ATTR void encoderRotateInterrupt() {
+  if(genericCount){
+    if(!genericDelay){    // This is because you can't do delays in functions nested too deep :(
+      if(digitalRead(DT)){
+        genericCouter--;
+      }else{
+        genericCouter++;
+      }
+      genericDelay = true;
+    }
+  }else if(isMenuOpen){
+    // Change the current menu option
+    if(digitalRead(DT)){
+      if (menuOption > 0) menuOption--;
+    }else{
+      if(currentMenuLength - menuOption - 1 > 0){
+        menuOption++;
+      }
+    }
+    
+    // Render menu based on the first menu option
+    if(menuOption - firstMenuOption >= rows){
+      firstMenuOption++;
+      renderMenu(currentMenu, firstMenuOption, false);
+    }else if(menuOption < firstMenuOption){
+      firstMenuOption--;
+      renderMenu(currentMenu, firstMenuOption, false);
+    }
+    lcd.setCursor(0, menuOption - firstMenuOption);
+    delay(100);
+  }
+}
+
+int abs(int x){
+  return x > 0 ? x : -x;
+}
+
+// 
+//  --- CALLBACKS ---
+//
+
+void updateTimeCallback(){
+  lcd.clear();
+  lcd.noCursor();
+  centerPrint("Updating time...", 1);
+
+  updateNTPTime();
+  closeMenu();
+}
+
+
+MenuItem alarmMenu[] = {
+  MenuItem("Back", alarmMenuBackCallback), MenuItem("Weekdays", setupAlarmDayCallback), MenuItem("Weekend", setupAlarmDayCallback), MenuItem("Monday", setupAlarmDayCallback), 
+  MenuItem("Tuesday", setupAlarmDayCallback),  MenuItem("Wednesday", setupAlarmDayCallback), MenuItem("Thursday", setupAlarmDayCallback), 
+  MenuItem("Friday", setupAlarmDayCallback), MenuItem("Saturday", setupAlarmDayCallback),   MenuItem("Sunday", setupAlarmDayCallback)
+};
+
+void setupAlarmCallback(){
+  changeMenu(alarmMenu, 10);
+}
+
+void setupAlarmDayCallback(){
+  int selectedDay = menuOption;
+  genericCount = true;
+  genericCouter = 0;
+  lcd.clear();
+  lcd.noCursor();
+  centerPrint("Alarm time");
+  delay(200);
+
+  int min = 0, h = 0;
+  char buffer[5]; // "0" "0" ":" "0" "0" "\0"
+  // Hour
+  do{
+    if(genericCouter < 0){
+      genericCouter = 23;
+    }
+    h = genericCouter % 24;
+    sprintf(buffer, "%02d:%02d", h, min);
+    centerPrint(buffer, 1);
+    delay(200);
+    genericDelay = false;
+  }while(digitalRead(SW));
+  genericCouter = 0;
+  
+  // Minutes
+  do{
+    if(genericCouter < 0){
+      genericCouter = 23;
+    }
+    min = genericCouter % 60;
+    sprintf(buffer, "%02d:%02d", h, min);
+    centerPrint(buffer, 1);
+    delay(200);
+    genericDelay = false;
+  }while(digitalRead(SW));
+
+  genericCount = false;
+  // TODO: Set the alarm and save to flash
+
+  changeMenu(alarmMenu, 10);
+}
+
+MenuItem mainMenu[] = {
+  MenuItem("Back", closeMenu), MenuItem("Setup alarm", setupAlarmCallback), MenuItem("Modify tmrw's alarm"), MenuItem("Change alarm sound"), MenuItem("Change LED settings"), MenuItem("Update time", updateTimeCallback)
+};
+
+void alarmMenuBackCallback(){
+  changeMenu(mainMenu, 6);
+}
+
+// Custom chars
+byte downArrow[] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B01110,
+  B00100
+};
 
 void setup() {
   Serial.begin(115200);  // Start serial communication at 115200 baud
@@ -115,6 +285,7 @@ void setup() {
   digitalWrite(DT, HIGH);
 
   lcd.init();
+  lcd.createChar(0, downArrow);
   toggleBacklight();
   centerPrint("Connecting...", 1);
 
@@ -125,19 +296,7 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(14), encoderRotateInterrupt, FALLING);
 }
 
-byte isMenuOpen = false;
-ICACHE_RAM_ATTR void encoderRotateInterrupt() {
-  if(isMenuOpen){
-    if(digitalRead(DT)){
-      Serial.println("+1");
-    }else{
-      Serial.println("-1");
-    }
-  }
-}
-
-
-const int NTPUpdateMillis = 1000 * 60 * 15;  // Update every 15 minutes
+const int NTPUpdateMillis = 1000 * 60 * 10;  // Update every 10 minutes
 
 long long int prev = 0;
 long long int lastTimeUpdate = -NTPUpdateMillis;
@@ -154,31 +313,31 @@ void loop() {
       updateNTPTime();
       lastTimeUpdate = millis();
     }
-    if (millis() - prev > 1000) {
-      // One second has passed
+  }else{
+    backlightTimer = millis();
+  }
+  if (millis() - prev > 1000) {
+    // One second has passed!
+    digitalWrite(ledPin, !digitalRead(ledPin));
 
-      digitalWrite(ledPin, !digitalRead(ledPin));
-      seconds += 1;
-      if (seconds == 60) {
-        seconds = 0;
-        minutes += 1;
-        if (minutes == 60) {
-          minutes = 0;
-          hours += 1;
-          if (hours == 24) {
-            hours = 0;
-            day = (day + 1) % 7;
-          }
+    seconds += 1;
+    if (seconds == 60) {
+      seconds = 0;
+      minutes += 1;
+      if (minutes == 60) {
+        minutes = 0;
+        hours += 1;
+        if (hours == 24) {
+          hours = 0;
+          day = (day + 1) % 7;
         }
       }
-
-      lcd.clear();
-
-      // The length of the time is always 8 chars with seconds
-      lcd.setCursor(calculateCenterTextColumnStart(8), 0);
-      lcd.printf("%02d:%02d:%02d", hours, minutes, seconds);
-      prev = millis();
     }
+
+    if(!isMenuOpen){
+      drawMainScreen();
+    }
+    prev = millis();
   }
 
   // Menu logic
@@ -189,8 +348,11 @@ void loop() {
       toggleBacklight();
       backlightTimer = millis();
     }else{
-      isMenuOpen = true;
-      renderMenu();
+      if(!isMenuOpen){
+        changeMenu(mainMenu, 6);
+      }else{
+        currentMenu[menuOption].executeCallback();
+      }
     }
     delay(200);
   }
